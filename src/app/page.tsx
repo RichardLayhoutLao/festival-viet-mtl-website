6810659480cf97092338748f3677ef1c01b81e7b
Richie <Richard.layhout.lao@gmail.com>
2024-03-16 16:47:50 -0400

    Add logo and place components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,31 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-import { NavigationMenuDemo } from "./header";
-
+} from '@/components/ui/navigation-menu';
+import { Header } from '../components/header';
+import Image from 'next/image';
+import logo from '../../public/logo.svg';
 
 export default function Home() {
   return (
-    <main>
-      <NavigationMenuDemo></NavigationMenuDemo>
-{/*       
-      <div>About</div>
-      <div>FAQ</div>
-      <div>APPLY</div>
-      <div>ÉDITION 2023</div>
-      <div>CONTACT</div>
-      <div>EN</div>
-      <div>FESTIVAL VIET MONTRÉAL</div>
+    <main className="container mx-auto">
+      <div className='flex items-center justify-between py-6'>
+        <Image
+          priority
+          src={logo}
+          height={310}
+          width={200}
+          alt='Follow us on Twitter'
+        />
+        <Header></Header>
+      </div>
+            
+      <div>HELLO WORLD</div>
+      {/* <div>FESTIVAL VIET MONTRÉAL</div>
       <div>11-JUILLET 2024</div>
       <div>ESPLANADE DU STADE OLYMPIQUE</div>
-      <div>DE 16H À 23H</div>
-      <div>FACEBOOK</div>
+      <div>DE 16H À 23H</div> */}
+      {/* <div>FACEBOOK</div>
       <div>INSTAGRAM</div>
       <div>YOUTUBE</div>
       <div>MAIL</div> */}
