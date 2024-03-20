01f41075055c32be6e19746aa7e6a9933d722f94
Richie <53919513+RichardLayhoutLao@users.noreply.github.com>
2024-03-20 00:28:27 -0400

    Feature/navbar (#3)
    * Create navbar components
    
    * Add logo and place components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from '@/components/ui/navigation-menu';
+import { Header } from '../components/header';
+import Image from 'next/image';
+import logo from '../../public/logo.svg';
 
 export default function Home() {
   return (
-    <main>
-      <Button>Hello world</Button>
+    <main className="container mx-auto">
+      <div className='flex items-center justify-between py-6'>
+        <Image
+          priority
+          src={logo}
+          height={310}
+          width={200}
+          alt='Follow us on Twitter'
+        />
+        <Header></Header>
+      </div>
+            
+      <div>HELLO WORLD</div>
+      {/* <div>FESTIVAL VIET MONTRÉAL</div>
+      <div>11-JUILLET 2024</div>
+      <div>ESPLANADE DU STADE OLYMPIQUE</div>
+      <div>DE 16H À 23H</div> */}
+      {/* <div>FACEBOOK</div>
+      <div>INSTAGRAM</div>
+      <div>YOUTUBE</div>
+      <div>MAIL</div> */}
     </main>
   );
 }
